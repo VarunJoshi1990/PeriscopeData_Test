65b9bd04c95601941f5f5c5a42db58a40a4776c7
Varun Joshi <vjoshi4@asu.edu>
2016-04-18 16:16:53 -0700

    Updated the comments

--- Periscope_Test.java
+++ Periscope_Test.java
@@ -6,22 +6,26 @@ public class Periscope_Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		//Input file
 		File f = new File("./input.txt");
 		Scanner s;
 		try {
 			s = new Scanner(f);
-
+			//Range of co-ordinates
 			int m = s.nextInt();
 			int n = s.nextInt();
 			boolean[][] ds = new boolean[m][n];
 			int count = 0;
+			//starting points
 			int x = s.nextInt();
 			int y = s.nextInt();
+			//The dirty spots
 			while (s.hasNextInt()) {
 				int i = s.nextInt();
 				int j = s.nextInt();
 				ds[i][j] = true;
 			}
+			//Initial spot is dirty??
 			if (ds[x][y] == true) {
 				ds[x][y] = false;
 				count++;
@@ -29,6 +33,7 @@ public class Periscope_Test {
 			s.nextLine();
 			String st = s.nextLine();
 			int i = 0;
+			//Traverse the directions and check for spots
 			while (i < st.length()) {
 				if (st.charAt(i) == 'N' && y < m - 1) {
 					y++;
@@ -45,6 +50,7 @@ public class Periscope_Test {
 				}
 				i++;
 			}
+			//Final Output
 			System.out.println(x + " " + y);
 			System.out.println(count);
 		} catch (FileNotFoundException e) {
